c7b9cd3215338570794f3ef9750cafc3cccca861
apemint <escobar.andres.alfonso@gmail.com>
2021-05-14 20:58:46 -0400

    reverted some changes when setting up db changes

--- models/card.js
+++ models/card.js
@@ -2,33 +2,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // //change this
-// const cardSchema = new Schema({
-//   name: { type: String, required: true },
-//   cmc: { type: Number, required: true },
-//   colors: Array,
-//   imageUrl: {type: String, required: false},
-//   set: { type: String, required: true}
-// });
+const cardSchema = new Schema({
+  name: { type: String, required: true },
+  cmc: { type: Number, required: true },
+  colors: Array,
+  imageUrl: {type: String, required: false},
+  set: { type: String, required: true}
+});
 
-// const Card = mongoose.model("Card", cardSchema);
+const Card = mongoose.model("Card", cardSchema);
 
-// module.exports = Card;
+module.exports = Card;
 
 
 //user schema
-const userSchema = new Schema({
-  displayName : {type: String, required: true, unique: true},
-  email: { type: String, required: true, unique: true},
-  cards : [
-    {
-      name: {type: String, required: true },
-      cmc: { type: Number, required: true },
-      colors: Array,
-      imageUrl: {type: String, required: false},
-      set: { type: String, required: true }
-    }
-  ]
-})
+// const userSchema = new Schema({
+//   displayName : {type: String, required: true, unique: true},
+//   email: { type: String, required: true, unique: true},
+//   cards : [
+//     {
+//       name: {type: String, required: true },
+//       cmc: { type: Number, required: true },
+//       colors: Array,
+//       imageUrl: {type: String, required: false},
+//       set: { type: String, required: true }
+//     }
+//   ]
+// })
 
-const User = mongoose.model("User", userSchema);
-module.exports = User; 
+// const User = mongoose.model("User", userSchema);
+// module.exports = User; 
