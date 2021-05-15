c9daad9cd076cefbdd87433578f3647f9c3d2311
apemint <escobar.andres.alfonso@gmail.com>
2021-05-14 22:18:11 -0400

    mongo atlas

--- server.js
+++ server.js
@@ -11,19 +11,26 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+  app.use(express.static("client/build"));
+}
 
-  // Add routes, both API and view
+// Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 // ///////////////////////////////////////////////////////////////////
 //----   CHANGE DB UNDER HERE ---
 ///////////////////////////////////////////////////////////////////////
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mycards");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mycards",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
